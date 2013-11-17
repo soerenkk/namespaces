b720fabb44f73fa4664c97f7fa335f2c5a87f7ca
soerenkk <soerenkk@users.noreply.github.com>
2013-11-17 19:20:27 +0100

    Create core.php

---
+++ core.php
@@ -0,0 +1,9 @@
+<?php
+
+// Require the autoloader \\
+require_once('OOP/Autoloader.php');
+
+// The autoloader have a namespace declared which allow us to to the following \\
+OOP\Autoloader::register();
+
+?>
